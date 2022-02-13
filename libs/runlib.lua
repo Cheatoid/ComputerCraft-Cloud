57cff5f67a5e46ebeaab5ffcfdb1a7b5186ea4f5
Cheatoid <Cheatoid@users.noreply.github.com>
2022-02-13 01:38:43 +0100

    Update runlib
    * Use colon as separator instead of slash
    
    * Localize function calls
    
    * Write documentation

--- libs/runlib.lua
+++ libs/runlib.lua
@@ -1,8 +1,12 @@
+local string_match, string_format, http_get, load, pcall = string.match, string.format, http.get, load, pcall
+
+--- Dynamically fetch and execute a Lua script (from GitHub repository).
+---@param libraryId string Library identifier (in 'AuthorName:LibraryName' format).
 _G.runlib = function(libraryId)
   assert(type(libraryId) == "string", "Library ID argument is missing")
-  local author, libraryName = string.match(libraryId, "^([%w-]+)/([%w-]+)$")
-  assert(libraryName, "Library ID must be in 'AuthorName/LibraryName' format")
-  local request = http.get(string.format("https://raw.github.com/%s/ComputerCraft-Cloud/main/libs/%s.lua", author, libraryName))
+  local author, libraryName = string_match(libraryId, "^([%w-]+):([%w-]+)$")
+  assert(libraryName, "Library ID must be in 'AuthorName:LibraryName' format")
+  local request = http_get(string_format("https://raw.github.com/%s/ComputerCraft-Cloud/main/libs/%s.lua", author, libraryName))
   if request then
     local contents = request.readAll()
     request.close()
